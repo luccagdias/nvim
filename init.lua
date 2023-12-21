af1942024e308e36704f20c8c1471693eba05902
Lucca Dias <lucca.gdias07@gmail.com>
2023-12-21 19:54:06 -0300

    Plugin and colorscheme configs

--- init.lua
+++ init.lua
@@ -1,1 +1,13 @@
 require "options"
+require "plugins"
+
+
+-- Noirbuddy colorsheme config
+require('noirbuddy').setup {
+    -- preset = 'kiwi'
+    -- preset = 'slate'
+    -- preset = 'minimal'
+    -- preset = 'crt-green'
+    -- preset = 'crt-amber'
+    preset = 'miami-nights'
+}
